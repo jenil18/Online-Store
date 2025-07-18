f0b6f635906b4da4eaa8d8cc4360b5940d8fd763
AYUSH PATEL <ayushgajera094@gmail.com>
2025-07-18 15:18:32 +0530

    discount

--- backend/product/models.py
+++ backend/product/models.py
@@ -9,9 +9,15 @@ class Product(models.Model):
     image = models.ImageField(upload_to='products/')
     description = models.TextField(blank=True, null=True)
     stock = models.PositiveIntegerField(default=0, help_text="Available quantity in stock")
-    original_price = models.DecimalField(max_digits=10, decimal_places=2, help_text="MRP (Original Price)")
-    discount_percent = models.PositiveIntegerField(help_text="Discount percentage (e.g., 79 for 79%)")
-    discounted_price = models.DecimalField(max_digits=10, decimal_places=2, help_text="Price after discount")
+    original_price = models.DecimalField(
+        max_digits=10, decimal_places=2, help_text="MRP (Original Price)", default=0, blank=True, null=True
+    )
+    discount_percent = models.PositiveIntegerField(
+        help_text="Discount percentage (e.g., 79 for 79%)", default=0, blank=True, null=True
+    )
+    discounted_price = models.DecimalField(
+        max_digits=10, decimal_places=2, help_text="Price after discount", default=0, blank=True, null=True
+    )
 
     def __str__(self):
         return self.name
