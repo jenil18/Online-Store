4682591491c5f3991e91b39aef1e5de4057b308c
jenil18 <jenilgajera19@gmail.com>
2025-07-22 15:08:59 +0530

    redux store fixed

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,37 @@
 import { configureStore } from '@reduxjs/toolkit';
 import shopOrderReducer from './shopOrderSlice';
 
+// LocalStorage persistence helpers
+const SHOP_ORDER_STORAGE_KEY = 'shopOrderReduxState';
+
+function loadShopOrderState() {
+  try {
+    const serializedState = localStorage.getItem(SHOP_ORDER_STORAGE_KEY);
+    if (!serializedState) return undefined;
+    return { shopOrder: JSON.parse(serializedState) };
+  } catch (e) {
+    return undefined;
+  }
+}
+
+function saveShopOrderState(state) {
+  try {
+    const serializedState = JSON.stringify(state.shopOrder);
+    localStorage.setItem(SHOP_ORDER_STORAGE_KEY, serializedState);
+  } catch (e) {
+    // Ignore write errors
+  }
+}
+
 const store = configureStore({
   reducer: {
     shopOrder: shopOrderReducer,
   },
+  preloadedState: loadShopOrderState(),
+});
+
+store.subscribe(() => {
+  saveShopOrderState(store.getState());
 });
 
 export default store; 
